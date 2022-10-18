54e052f388c3f20a0be2ab995b0f0c6ae0d5336d
Xiaying Peng <xiaying.peng@coinbase.com>
2022-10-18 12:00:42 -0700

    test rosetta api

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -2,15 +2,15 @@
 
 nohup make run-rosetta > /dev/null 2>&1 &
 
-sleep 180
+sleep 10s
 
-block_tip=($(curl -s --location --request POST 'http://localhost:8000/network/status' \
+block_tip=($(curl -s --location --request POST 'http://localhost:8080/network/status' \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "network_identifier": {
         "blockchain": "Ethereum",
         "network": "Mainnet"
     }
-}' | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["sync_status"]["target_index"])'))
+}' | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["current_block_identifier"]["index"])'))
 
 echo $(seq $START_INDEX $BLKS_PER_CHUNK $block_tip | jq -cnR '[inputs | select(length>0)]')
