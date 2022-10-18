f6f4f2ec1f4ab4c9561f33436d65cdf8701c449f
Xiaying Peng <xiaying.peng@coinbase.com>
2022-10-18 16:12:49 -0700

    add genesis file

--- .github/scripts/populate_txns.py
+++ .github/scripts/populate_txns.py
@@ -1,17 +1,19 @@
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
+from eth_account import Account
 
 web3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8546"))
 web3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 print("latest block", web3.eth.block_number)
 
+dst = Account.create('KEYSMASH FJAFJKLDSKF7JKFDJ 2121')
+
 for _ in range(5):
-    # transfer 12345 to account 0
     web3.eth.send_transaction({
-    'to': web3.eth.accounts[0],
-    'from': web3.eth.coinbase,
-    'value': 12345
+        'to': dst.address,
+        'from': web3.eth.coinbase,
+        'value': 10000
     })
 
 print("latest block", web3.eth.block_number)
