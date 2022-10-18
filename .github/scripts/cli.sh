41d456ce8efae38337fdf9b69d9863ac09cc6e3c
Xiaying Peng <xiaying.peng@coinbase.com>
2022-10-18 11:04:39 -0700

    t

--- .github/scripts/cli.sh
+++ .github/scripts/cli.sh
@@ -3,13 +3,13 @@
 END_INDEX=$(($START_INDEX+$COUNT))
 echo "index ranges", $START_INDEX, $END_INDEX
 
-cd examples/ethereum
-nohup make run-rosetta > /dev/null 2>&1 &
+# cd examples/ethereum
+# nohup make run-rosetta > /dev/null 2>&1 &
 
-# downloading cli
-curl -sSfL https://raw.githubusercontent.com/coinbase/rosetta-cli/master/scripts/install.sh | sh -s
+# # downloading cli
+# curl -sSfL https://raw.githubusercontent.com/coinbase/rosetta-cli/master/scripts/install.sh | sh -s
 
-sleep 180
+# sleep 180
 
-echo "start check:data"
-./bin/rosetta-cli --configuration-file examples/ethereum/rosetta-cli-conf/mainnet/config.json check:data --start-block $START_INDEX --end-block $END_INDEX
+# echo "start check:data"
+# ./bin/rosetta-cli --configuration-file examples/ethereum/rosetta-cli-conf/mainnet/config.json check:data --start-block $START_INDEX --end-block $END_INDEX
